2543905b7154529033cf1b4aa939ad45ec933ae6
Vince Reginald Eugenio <112544057+notVRE@users.noreply.github.com>
2025-10-06 12:07:35 +0800

    Add files via upload
    final update

---
+++ areacode.js
@@ -0,0 +1,16 @@
+// areaCodes.js
+const areaCodes = {
+  "201": { state: "New Jersey", tz: ["America/New_York"] },
+  "202": { state: "District of Columbia", tz: ["America/New_York"] },
+  "203": { state: "Connecticut", tz: ["America/New_York"] },
+  "208": { state: "Idaho", tz: ["America/Boise", "America/Los_Angeles"] },
+  "812": { state: "Indiana", tz: ["America/New_York", "America/Chicago"] },
+  "701": { state: "North Dakota", tz: ["America/Chicago", "America/Denver"] },
+  "850": { state: "Florida", tz: ["America/New_York", "America/Chicago"] },
+  "602": { state: "Arizona", tz: ["America/Phoenix"] },
+  "702": { state: "Nevada", tz: ["America/Los_Angeles"] },
+  "212": { state: "New York", tz: ["America/New_York"] }
+  // Add more area codes here...
+};
+
+const notAvailableStates = ["Nevada", "Arizona", "Connecticut"];
